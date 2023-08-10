3b9e89fee04847cf2d915ec62d4ff85fc407844f
daiyadeguchi <double.d.rugby@gmail.com>
2023-08-11 07:08:34 +0900

    maps shows

---
+++ frontend/halfway/src/app/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { useLoadScript, GoogleMap } from '@react-google-maps/api';
+import type { NextPage } from 'next';
+import { useMemo } from 'react';
+
+const Home: NextPage = () => {
+  const libraries = useMemo(() => ['places'], []);
+  const mapCenter = useMemo(
+    () => ({ lat: 35.652832, lng: 139.839478 }),
+    []
+  );
+
+  const mapOptions = useMemo<google.maps.MapOptions> (
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  )
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        options={{ scrollwheel:false }}
+        zoom={14}
+        center={mapCenter}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerStyle={{ width: '100%', height: '100vh' }}
+        onLoad={() => console.log('Map Componetn Loaded...')}
+      />
+    </div>
+  ); 
+};
+
+export default Home;
