7ed3b2f76d9668366d86af3bead9eea5e29cac4c
daiyadeguchi <double.d.rugby@gmail.com>
2023-09-06 17:55:14 +0900

    initialize frontend and create header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={"/"} className="nav-link">
+                  HALFWAY<br/>Meet Your Friend at Halfway Point
+                </Link>
+              </Navbar.Brand>
+            </Container>
+          </Navbar>
+        </header>
+        <Container>
+          <div className="wrapper">
+            <Routes>
+            </Routes>
+          </div> 
+        </Container>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
