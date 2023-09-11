513eca3f6e4b5afe0198a8e0030ed542f576f056
daiyadeguchi <double.d.rugby@gmail.com>
2023-09-12 06:53:20 +0900

    center form

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -9,9 +9,13 @@ const SidebarSearchField = () => {
 
   return (
     <div>
-        <form onSubmit={handleSubmit} className="d-flex align-items-center justify-content-center">
-          <input type="text" />
-          <input type="submit" value="FIND" />
+        <form>
+          <div className="row align-items-center justify-content-center mb-3">
+            <input type="text" className="col-md-8 col-sm-8 rounded" />
+          </div>
+          <div className="row align-items-center justify-content-center">
+            <input type="submit" value="FIND" className="col-md-6 col-sm-6 rounded" />
+          </div>
         </form>
     </div>
   )
