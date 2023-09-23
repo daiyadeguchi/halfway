20010dee4629d1618b3751961a4ba90380f4c57a
daiyadeguchi <double.d.rugby@gmail.com>
2023-09-24 07:24:34 +0900

    Shows location

--- frontend/src/Components/UserList.js
+++ frontend/src/Components/UserList.js
@@ -3,10 +3,16 @@ const UserList = (props) => {
   if(users.length > 0) {
     const userItem = users[0]
     const userItems = userItem.map((user) =>
-      <li key={user._id} className="mb-3 text-white">{user.username}</li>
+      <tr>
+        <td key={user._id} className="mb-3 text-white">{user.username}</td>
+        <td key={user._id} className="mb-3 text-white">{user.location}</td>
+      </tr>
     )
+
     return (
-      <ul>{userItems}</ul>
+      <table>
+        {userItems}
+      </table>
     )
   }
 }
