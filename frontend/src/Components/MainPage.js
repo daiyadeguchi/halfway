900dd66c5b1a56ee8d1b509c3f25e9dc4e36fe24
daiyadeguchi <double.d.rugby@gmail.com>
2023-09-29 06:44:01 +0900

    At least, sidebar's collapsible

--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import SidebarSearchField from "./Sidebar";
 import GoogleMapComponent from "./GoogleMapComponent";
+import { Offcanvas, Button } from "react-bootstrap";
 
 const MainPage = () => {
+  const [show, setShow] = useState(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="d-flex bg-dark align-items-center">
-      <div className="w-25">
-        <SidebarSearchField />
-      </div>
-      <div className="w-75">
+    <div>
+      <Button variant="primary" onClick={handleShow}>show</Button>
+      <Offcanvas show={show} onHide={handleClose} className="w-50 d-flex bg-dark align-items-center">
+        <Offcanvas.Header closeButton className="text-white">
+          <Offcanvas.Title>Title</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div className="d-flex bg-dark align-items-center">
+            <SidebarSearchField />
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+      <div className="w-100">
         <GoogleMapComponent />
       </div>
     </div>
