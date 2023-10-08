f4af3621fb129b1946ed385aedf06d9115968497
daiyadeguchi <double.d.rugby@gmail.com>
2023-10-09 07:33:55 +0900

    Implement multiple location pin

--- frontend/src/Components/GoogleMapComponent.js
+++ frontend/src/Components/GoogleMapComponent.js
@@ -31,23 +31,43 @@ const GoogleMapComponent = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
     libraries: libraries,
-  });
+  })
 
   let center = {
     lat: users.length > 0 ? parseFloat(users[0].lat) : 35.652832,
     lng: users.length > 0 ? parseFloat(users[0].lng) : 139.839478,
   };
+
+  if(users.length > 0) {
+    const centerMarkers = users.map((user) => 
+      <Marker position={{lat: parseFloat(user.lat), lng: parseFloat(user.lng)}}/>
+    )
+
+    return isLoaded ? (
+      <GoogleMap
+        center={center}
+        zoom={14}
+        options={mapOptions}
+        mapContainerStyle={{ width: '100%', height: 'calc(100vh - 86px)' }}
+      >
+        {centerMarkers}
+      </GoogleMap> 
+    ): <></>;
+  }
+
+
   
-  return isLoaded ? (
-    <GoogleMap
-      center={center}
-      zoom={14}
-      options={mapOptions}
-      mapContainerStyle={{ width: '100%', height: 'calc(100vh - 86px)' }}
-    >
-      <Marker position={center} />
-    </GoogleMap>
-  ) : <></>;
+  // return isLoaded ? (
+  //   <GoogleMap
+  //     center={center}
+  //     zoom={14}
+  //     options={mapOptions}
+  //     mapContainerStyle={{ width: '100%', height: 'calc(100vh - 86px)' }}
+  //   >
+  //     <Marker position={center} />
+  //   </GoogleMap>
+  // ) : <></>;
+
 };
 
 export default GoogleMapComponent;
