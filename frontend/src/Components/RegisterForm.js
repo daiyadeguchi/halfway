4765b5061599b5f536c5db9e1f2644b270cf2251
daiyadeguchi <double.d.rugby@gmail.com>
2023-10-15 07:35:53 +0900

    Implement valdiations for RegisterForm

--- frontend/src/Components/RegisterForm.js
+++ frontend/src/Components/RegisterForm.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 import { usePlacesWidget } from "react-google-autocomplete";
 
 const RegisterForm = () => {
+  const [isInvalidInput , setIsInvalidInput] = useState(true)
+  const [buttonLabel, setButtonLabel] = useState("Enter Valid Info")
   const options = {
     fields: ["address_components", "geometry", "name"],
     types: ["train_station"],
@@ -10,7 +13,11 @@ const RegisterForm = () => {
 
   const { ref } = usePlacesWidget({
     apiKey: process.env.REACT_APP_MAPS_API_KEY,
-    options: options
+    options: options,
+    onPlaceSelected: () => {
+      setIsInvalidInput(false)
+      setButtonLabel("Register")
+    }
   })
   
   return (
@@ -18,8 +25,8 @@ const RegisterForm = () => {
       <div className="h-25 row d-flex align-items-center justify-content-center">
         <input type="text" name="username" className="form-control-lg col-sm-8 col-md-8 rounded" placeholder="Enter username" required/>
         <input type="text" name="location" className="form-control-lg col-sm-8 col-md-8 rounded" ref={ref} required/>
-        <Button variant="danger" size="lg" block="block" type="submit" className="col-md-6">
-          REGISTER 
+        <Button variant="danger" size="lg" block="block" type="submit" className="col-md-6" disabled={isInvalidInput}>
+          {buttonLabel}
         </Button>
       </div>
     </div>
